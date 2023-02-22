fe69d88c32119859870cb4748095a183c5622143
Lyianu <1066184520@qq.com>
2023-02-22 20:30:46 +0800

    add TestFileOpen and TestFileClose

--- sdfs/file_test.go
+++ sdfs/file_test.go
@@ -1,48 +1,55 @@
 package sdfs
 
-// func TestFileOpen(t *testing.T) {
-// 	f := NewFile("hello", "123", "/hello", "FileOpenTestFile")
-// 	wg := sync.WaitGroup{}
-// 	wg.Add(10)
-// 	var err error
-// 	for i := 0; i < 10; i++ {
-// 		go func() {
-// 			_, e := f.Open()
-// 			if e != nil {
-// 				err = e
-// 			}
-// 			wg.Done()
-// 		}()
-// 	}
-// 	wg.Wait()
-// 	if err == nil {
-// 		t.Fatalf("Expected File Not Found error, got nil")
-// 	}
-// 	if f.SemaphoreOpen != 10 {
-// 		t.Fatalf("SemaphoreOpen = %d, Expected: %d", f.SemaphoreOpen, 10)
-// 	}
-// }
+import (
+	"sync"
+	"testing"
+)
 
-// func TestFileClose(t *testing.T) {
-// 	f := NewFile("hello", "/hello", "FileOpenTestFile")
-// 	wg := sync.WaitGroup{}
-// 	wg.Add(10)
-// 	var err error
-// 	for i := 0; i < 10; i++ {
-// 		go func() {
-// 			_, e := f.Open()
-// 			if e != nil {
-// 				err = e
-// 			}
-// 			f.Close()
-// 			wg.Done()
-// 		}()
-// 	}
-// 	wg.Wait()
-// 	if err == nil {
-// 		t.Fatalf("Expected error, got nil")
-// 	}
-// 	if f.SemaphoreOpen != 0 {
-// 		t.Fatalf("SemaphoreOpen = %d, want %d", f.SemaphoreOpen, 0)
-// 	}
-// }
+func TestFileOpen(t *testing.T) {
+	fs := NewFS()
+	f := NewFile("hello", "123", "/hello", 0, fs.Roots[0])
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	var err error
+	for i := 0; i < 10; i++ {
+		go func() {
+			_, e := f.Open()
+			if e != nil {
+				err = e
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	if err == nil {
+		t.Fatalf("Expected File Not Found error, got nil")
+	}
+	if f.SemaphoreOpen != 10 {
+		t.Fatalf("SemaphoreOpen = %d, Expected: %d", f.SemaphoreOpen, 10)
+	}
+}
+
+func TestFileClose(t *testing.T) {
+	fs := NewFS()
+	f := NewFile("hello", "123", "/hello", 0, fs.Roots[0])
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	var err error
+	for i := 0; i < 10; i++ {
+		go func() {
+			_, e := f.Open()
+			if e != nil {
+				err = e
+			}
+			f.Close()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	if err == nil {
+		t.Fatalf("Expected error, got nil")
+	}
+	if f.SemaphoreOpen != 0 {
+		t.Fatalf("SemaphoreOpen = %d, want %d", f.SemaphoreOpen, 0)
+	}
+}
