c920e23a065b36630757525c9a36c51e2898fe45
ast <1066184520@qq.com>
2023-02-22 13:24:16 +0800

    add test for file open

--- sdfs/file_test.go
+++ sdfs/file_test.go
@@ -1,6 +1,32 @@
 package sdfs
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestFileOpen(t *testing.T) {
+	f := NewFile("hello", "/hello", "FileOpenTestFile")
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	var err error
+	for i := 0; i < 10; i++ {
+		go func() {
+			_, e := f.Open()
+			if e != nil {
+				err = e
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	if err == nil {
+		t.Fatalf("Expected File Not Found error, got nil")
+	}
+	if f.SemaphoreOpen != 10 {
+		t.Fatalf("SemaphoreOpen = %d, Expected: %d", f.SemaphoreOpen, 10)
+	}
+}
+
+func TestFileClose(t *testing.T) {
 }
