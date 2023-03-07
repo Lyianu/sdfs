209ead78b70bd08b9224ee903d5963ff6fd69633
Lyianu <1066184520@qq.com>
2023-03-07 13:52:26 +0800

    add election timer for raft

--- raft/raft.go
+++ raft/raft.go
@@ -1,23 +1,73 @@
 package raft
 
 import (
+	"math/rand"
+	"os"
 	"sync"
+	"time"
 )
 
 const (
 	FOLLOWER = iota
 	LEAD
-	CANDIDATEER
+	CANDIDATE
 )
+
+var maxRTT = 150
 
 type ConsensusModule struct {
 	id      int
 	peerIds []int
 	server  *Server
 
+	currentTerm int
+	votedFor    int
+
+	state int
+
+	electionResetEvent time.Time
+
 	mu sync.Mutex
 }
 
 func (cm *ConsensusModule) runElectionTimer() {
+	timeoutDuration := cm.electionTimeout()
+	cm.mu.Lock()
+	termStarted := cm.currentTerm
+	cm.mu.Unlock()
 
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+	for {
+		<-ticker.C
+
+		cm.mu.Lock()
+		if cm.state != CANDIDATE && cm.state != FOLLOWER {
+			// become LEADER
+			cm.mu.Unlock()
+			return
+		}
+
+		if termStarted != cm.currentTerm {
+			// Term changed
+			cm.mu.Unlock()
+			return
+		}
+
+		if elapsed := time.Since(cm.electionResetEvent); elapsed >= timeoutDuration {
+			// timeout occurs, become Candidate
+			// cm.startElection()
+			cm.mu.Unlock()
+			return
+		}
+		cm.mu.Unlock()
+	}
+}
+
+func (cm *ConsensusModule) electionTimeout() time.Duration {
+	if len(os.Getenv("RAFT_FORCE_MORE_REELECTION")) > 0 && rand.Intn(3) == 0 {
+		return time.Duration(maxRTT) * time.Millisecond
+	} else {
+		return time.Duration(maxRTT+rand.Intn(maxRTT)) * time.Millisecond
+	}
 }
