8e8ef47b07c58ea75e199c98400235a60e5bc43e
Lyianu <1066184520@qq.com>
2023-03-09 20:32:12 +0800

    add log.SetLevel

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/Lyianu/sdfs/node"
+import (
+	"github.com/Lyianu/sdfs/log"
+	"github.com/Lyianu/sdfs/node"
+)
 
 func main() {
+	log.SetLevel(log.DEBUG)
 	n := node.NewNode(":8080")
 	n.Start()
 }
--- log/log.go
+++ log/log.go
@@ -3,22 +3,27 @@
 package log
 
 import (
+	"io"
 	"log"
 	"os"
+	"sync"
 )
 
 // log levels
 const (
-	DISABLED = iota
-	DEBUG
+	DEBUG = iota
 	INFO
 	ERROR
+	DISABLED
 )
 
 var (
 	debugLog = log.New(os.Stdout, "\033[37m[DEBUG]\033[0m", log.LstdFlags)
 	errorLog = log.New(os.Stdout, "\033[41m[ERROR]\033[0m", log.LstdFlags|log.Lshortfile)
 	infoLog  = log.New(os.Stdout, "\033[0;34m[INFO ]\033[0m", log.LstdFlags)
+
+	loggers = []*log.Logger{debugLog, errorLog, infoLog}
+	mu      sync.Mutex
 )
 
 var (
@@ -33,4 +38,25 @@ var (
 // messaging middleware
 func message(v interface{}) {
 
+}
+
+func SetLevel(level int) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for _, logger := range loggers {
+		logger.SetOutput(os.Stdout)
+	}
+
+	if DEBUG < level {
+		debugLog.SetOutput(io.Discard)
+	}
+
+	if INFO < level {
+		infoLog.SetOutput(io.Discard)
+	}
+
+	if ERROR < level {
+		errorLog.SetOutput(io.Discard)
+	}
 }
