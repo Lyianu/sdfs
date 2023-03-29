3000f6bc3e42c105328d137beb17e5923d18e1fa
ast <1066184520@qq.com>
2023-03-29 11:05:56 +0800

    refactor PQueue

--- pkg/pqueue/pqueue.go
+++ pkg/pqueue/pqueue.go
@@ -1,11 +1,22 @@
 package pqueue
 
+import "sync"
+
+// PQueue is a priority queue implementation, it is concurrent safe
 type PQueue struct {
+	root   *pQueueNode
+	length int
+
+	mu sync.RWMutex
 }
 
 type pQueueNode struct {
 	root           Priorityer
 	lChild, rChild *pQueueNode
+}
+
+func NewPQueue() *PQueue {
+	return &PQueue{}
 }
 
 func newPQueueNode() *pQueueNode {
@@ -16,18 +27,30 @@ func newPQueueNode() *pQueueNode {
 	}
 }
 
+// Priorityer tells the queue item's priority
 type Priorityer interface {
 	Priority() int
 }
 
+// Push adds an item to the queue,
+// the item must implement the Priorityer interface
 func (p *PQueue) Push(pri Priorityer) {
 
 }
 
+// First returns the highest priority item in the queue without removing it,
+// if the queue is empty, nil is returned
+func (p *PQueue) First() Priorityer {
+	return nil
+}
+
+// Pop returns the highest priority item in the queue, if the queue is empty,
+// nil is returned
 func (p *PQueue) Pop() Priorityer {
 	return nil
 }
 
+// Len returns the number of items in the queue
 func (p *PQueue) Len() int {
 	return 0
 }
